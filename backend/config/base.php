f24ba41479b5f368055309e2f57271efbdc44251
叶清浩 <qinghao.ye@g-emall.com>
2017-09-07 14:46:27 +0800

    new

---
+++ backend/config/base.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'id' => 'backend',
+    'basePath' => dirname(__DIR__),
+    'sourceLanguage'=>'zh-CN',
+    'language'=>'zh-CN',
+    'components' => [
+        'urlManager' => require __DIR__.'/_urlManager.php',
+        'frontendCache' => require Yii::getAlias('@frontend/config/_cache.php')
+    ],
+];
